8d4f0f3e6ec05ee2b6d05d094ba13c7aaa41e4b1
ElCholoGamer <josedanielgrayson@gmail.com>
2021-02-18 21:54:30 -0500

    move this import

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,9 @@ import BoneElevator from './attacks/bone-elevator';
 import HorizontalBoneSprite from './assets/img/horizontal-bone.png';
 import VerticalBoneSprite from './assets/img/vertical-bone.png';
 import HeartSprite from './assets/img/heart.png';
-import './index.css';
 import BoneGap from './attacks/bone-gap';
 import BoneCurve from './attacks/bone-curve';
+import './index.css';
 
 Math.clamp = function (n, min, max) {
 	return Math.max(min, Math.min(n, max));
